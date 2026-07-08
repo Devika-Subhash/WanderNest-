eefa565b203d8f45b97f6b880c46bfac235ed9d4
Devika Subhash <devikasubhash024@gmail.com>
2026-07-08 21:22:56 +0530

    Set up React Router and initial page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Destinations from "./pages/Destinations";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
-    <h1>Welcome to WanderNest ✈️</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Destinations.jsx
@@ -0,0 +1,5 @@
+function Destinations() {
+  return <h1>Destinations</h1>;
+}
+
+export default Destinations;
